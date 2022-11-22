0eb0763d2729b2fcd5680eae1205fc06d2220fe3
Ning Sun <sunng@protonmail.com>
2022-11-22 20:25:38 +0800

    ci: do not use exit code to pass condition

--- update.sh
+++ update.sh
@@ -4,7 +4,6 @@ set -e
 export tagdate=$(echo $tag | cut -d - -f 3)
 export sha256sum=$(curl -L https://github.com/GreptimeTeam/greptimedb/releases/download/$tag/greptime-linux-amd64.sha256sum)
 export pkgver=0.1.0.$tagdate
-export source="https://github.com/GreptimeTeam/greptimedb/releases/download/${tag}/greptime-linux-amd64.tgz"
 
 sed -i -r s/^pkgver=.+/pkgver=$pkgver/ PKGBUILD
 sed -i -r s/^_pkgvergh=.+/_pkgvergh=$tag/ PKGBUILD
@@ -14,4 +13,6 @@ sed -i -r s/pkgver.+/pkgver\ =\ $pkgver/ .SRCINFO
 sed -i -r s/download\\/.+\\//download\\/$tag\\// .SRCINFO
 sed -i -r s/sha256sums.+/sha256sums\ =\ $sha256sum/ .SRCINFO
 
-git diff --no-ext-diff --quiet --exit-code
+if [[ -n $(git status --porcelain) ]]; then
+    echo "dirty=yes" >>$GITHUB_OUTPUT
+fi
